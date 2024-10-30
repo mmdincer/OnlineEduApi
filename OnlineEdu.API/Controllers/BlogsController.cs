c340db017fe6ef3319e382d34873c2bf2823231e
mmdincer <mmdincer@outlook.com>
2024-10-31 01:12:31 +0300

    blog, blogCategories, contact, course, courseCategories controllerları eklendi

---
+++ OnlineEdu.API/Controllers/BlogsController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.Business.Abstract;
+using OnlineEdu.DTO.DTOs.BannerDtos;
+using OnlineEdu.DTO.DTOs.BlogDtos;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController(IGenericService<Blog> _blogService, IMapper _mapper) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var values = _blogService.TGetList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var value = _blogService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateBlogDto createBlogDto)
+        {
+            var newValues = _mapper.Map<Blog>(createBlogDto);
+            _blogService.TCreate(newValues);
+            return Ok("Blog oluşturuldu.");
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            _blogService.TDelete(id);
+            return Ok("Blog silindi.");
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateBlogDto updateBlogDto)
+        {
+            var values = _mapper.Map<Blog>(updateBlogDto);
+            _blogService.TUpdate(values);
+            return Ok("Blog güncellendi");
+        }
+    }
+}
