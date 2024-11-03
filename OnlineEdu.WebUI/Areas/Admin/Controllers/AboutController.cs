a86143d9ff76bd605df6dae920963b7a01bc37ac
mmdincer <mmdincer@outlook.com>
2024-11-03 18:53:48 +0300

    area oluşturuldu

---
+++ OnlineEdu.WebUI/Areas/Admin/Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEdu.WebUI.Areas.Admin.Controllers
+{
+    public class AboutController : Controller
+    {
+        [Route("[area]/[controller]/[action]/{id?}")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
