fc919f4ef11230bf641e7169a9e5fed1cd0e2d4e
mmdincer <mmdincer@outlook.com>
2024-11-03 18:43:16 +0300

    messages, sociaMedia, subscriber, testimonial controllerlarının oluşturulması

---
+++ OnlineEdu.API/Controllers/SocialMediasController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.Business.Abstract;
+using OnlineEdu.DTO.DTOs.AboutDtos;
+using OnlineEdu.DTO.DTOs.SocialMediaDtos;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocialMediasController(IGenericService<SocialMedia> _socialMediaService, IMapper _mapper) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var values = _socialMediaService.TGetList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var value = _socialMediaService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            _socialMediaService.TDelete(id);
+            return Ok("SocialMedia Alanı silindi.");
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateSocialMediaDto createSocialMediaDto)
+        {
+            var newValue = _mapper.Map<SocialMedia>(createSocialMediaDto);
+            _socialMediaService.TCreate(newValue);
+            return Ok("Yeni SocialMedia alanı oluşturuldu.");
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateSocialMediaDto updateSocialMediaDto)
+        {
+            var newValue = _mapper.Map<SocialMedia>(updateSocialMediaDto);
+            _socialMediaService.TUpdate(newValue);
+            return Ok("SocialMedia alanı güncelleştirildi");
+        }
+    }
+}
---
+++ OnlineEdu.API/Controllers/TestimonialsController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.Business.Abstract;
+using OnlineEdu.DTO.DTOs.AboutDtos;
+using OnlineEdu.DTO.DTOs.TestimonialDtos;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialsController(IGenericService<Testimonial> _testimonialService, IMapper _mapper) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var values = _testimonialService.TGetList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var value = _testimonialService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            _testimonialService.TDelete(id);
+            return Ok("Testimonial Alanı silindi.");
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateTestimonialDto createTestimonialDto)
+        {
+            var newValue = _mapper.Map<Testimonial>(createTestimonialDto);
+            _testimonialService.TCreate(newValue);
+            return Ok("Yeni Testimonial alanı oluşturuldu.");
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateTestimonialDto updateTestimonialDto)
+        {
+            var newValue = _mapper.Map<Testimonial>(updateTestimonialDto);
+            _testimonialService.TUpdate(newValue);
+            return Ok("Testimonial alanı güncelleştirildi");
+        }
+    }
+}
